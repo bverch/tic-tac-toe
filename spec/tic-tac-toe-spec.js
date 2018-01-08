98f34e850ee92f72e60742a45848a771f9bbeb6b
Brendon Verch <bverch24@gmail.com>
2018-01-08 15:30:06 -0800

    test functionality, detect if a victory has occurred

---
+++ spec/tic-tac-toe-spec.js
@@ -0,0 +1,47 @@
+var chai = require('chai');
+var ticTacToe = require('../tic-tac-toe.js');
+var assert = require('assert');
+
+describe('basic functionality', function() {
+  it ('should initialize an empty board of size 3', function() {
+    var myBoard = new ticTacToe.Board;
+    assert.equal(myBoard.storage.length, 3);
+    assert.equal(myBoard.storage[0].length, 3);
+    assert.equal(myBoard.storage[1].length, 3);
+    assert.equal(myBoard.storage[2].length, 3);
+  });
+  it ('should make a move on a board', function() {
+    var myBoard = new ticTacToe.Board;
+    myBoard.move(0, 1);
+    assert.equal(myBoard.storage[0][1], 'x');
+  });
+  it ('should alternate moves between players', function() {
+    var myBoard = new ticTacToe.Board;
+    myBoard.move(0, 1);
+    assert.equal(myBoard.storage[0][1], 'x');
+    myBoard.move(0, 0);
+    assert.equal(myBoard.storage[0][0], 'o');
+    myBoard.move(1, 0);
+    assert.equal(myBoard.storage[1][0], 'x');
+  });
+  it ('should not overwrite moves that have already happened', function() {
+    var myBoard = new ticTacToe.Board;
+    myBoard.move(0, 1);
+    assert.equal(myBoard.storage[0][1], 'x');
+    myBoard.move(0, 1);
+    assert.equal(myBoard.storage[0][1], 'x');
+    myBoard.move(0, 0);
+    assert.equal(myBoard.storage[0][0], 'o');
+  });
+});
+describe('winning', function() {
+  it ('should detect if a victory has occurred', function() {
+    var myBoard = new ticTacToe.Board;
+    myBoard.move(0, 0);
+    myBoard.move(1, 0);
+    myBoard.move(0, 1);
+    myBoard.move(2, 0);
+    myBoard.move(0, 2);
+    assert.equal(myBoard.checkVictory(), true);
+  });
+});
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,20 +3,66 @@ var Board = function(n = 3) {
   this.player = 'x';
   for (var i = 0; i < this.storage.length; i++) {
     this.storage[i] = new Array(n);
+    this.storage[i].fill('');
   }
 }
 
 Board.prototype.move = function(i, j){
-  this.storage[i][j] = this.player;
-  this.changePlayers();
+  if (!this.storage[i][j]) {
+    this.storage[i][j] = this.player;
+    this.changePlayers();
+  }
+  this.printBoard();
 }
 
 Board.prototype.changePlayers = function() {
   this.player = this.player === 'x' ? 'o':'x';
 }
 
-var myBoard = new Board();
-myBoard.move(0,0);
-myBoard.move(0,2);
-myBoard.move(1,2);
-console.log(myBoard.storage);
+Board.prototype.printBoard = function() {
+  for (var i = 0; i < this.storage.length; i++) {
+    var boardRow = '';
+    for (var j = 0; j < this.storage.length; j++) {
+      if (this.storage[i][j]) {
+        boardRow += this.storage[i][j];
+      } else {
+        boardRow += '_';
+      }
+    }
+    console.log(boardRow);
+  }
+}
+
+Board.prototype.checkVictory = function() {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[0][i] === 'x' && this.storage[1][i] === 'x' && this.storage[2][i] === 'x') {
+      return true;
+    }
+    if (this.storage[i][0] === 'x' && this.storage[i][1] === 'x' && this.storage[i][2] === 'x') {
+      return true;
+    }
+  }
+  if (this.storage[0][0] === 'x' && this.storage[1][1] === 'x' && this.storage[2][2] === 'x') {
+    return true;
+  }
+  if (this.storage[0][2] === 'x' && this.storage[1][1] === 'x' && this.storage[2][0] === 'x') {
+    return true;
+  }
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[0][i] === 'o' && this.storage[1][i] === 'o' && this.storage[2][i] === 'o') {
+      return true;
+    }
+    if (this.storage[i][0] === 'o' && this.storage[i][1] === 'o' && this.storage[i][2] === 'o') {
+      return true;
+    }
+  }
+  if (this.storage[0][0] === 'o' && this.storage[1][1] === 'o' && this.storage[2][2] === 'o') {
+    return true;
+  }
+  if (this.storage[0][2] === 'o' && this.storage[1][1] === 'o' && this.storage[2][0] === 'o') {
+    return true;
+  }
+  return false;
+}
+
+exports.Board = Board;
